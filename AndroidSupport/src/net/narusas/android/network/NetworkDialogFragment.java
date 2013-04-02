082848c0898ddb5c63cd4b904aaa5b4edb31b0b4
Jisung, Ahn <narusas@gmail.com>
2013-04-02 18:18:09 +0900

    테스트 커밋

---
+++ AndroidSupport/src/net/narusas/android/network/NetworkDialogFragment.java
@@ -0,0 +1,13 @@
+package net.narusas.android.network;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class NetworkDialogFragment extends DialogFragment {
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateDialog(savedInstanceState);
+	}
+}
